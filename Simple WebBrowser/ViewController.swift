499b57ae5cc09076b11dd696271cb302da4cbf5c
Testing_author <testing_beta@mail.com>
2020-02-02 18:56:34 +0200

    Initial Commit

---
+++ Simple WebBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple WebBrowser
+//
+//  Created by DDDD on 02/02/2020.
+//  Copyright © 2020 MeerkatWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
