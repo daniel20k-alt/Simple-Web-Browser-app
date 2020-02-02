c742a2d090dc5502c308644a145c24365ec1cfa2
Testing_author <testing_beta@mail.com>
2020-02-02 22:00:50 +0200

    Allowing users to open websites that are predifined, Adding the option to choose from the selected websites.

--- Simple WebBrowser/ViewController.swift
+++ Simple WebBrowser/ViewController.swift
@@ -21,11 +21,33 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "New page", style: .plain, target: self, action: #selector(openTapped))
+        
+        
         let url = URL(string: "https://www.apple.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
 
-
+    
+    // introducing the function which allows to open pre-defined websites
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "bbc.com", style: .default, handler: openPage))
+    ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
 }
 
