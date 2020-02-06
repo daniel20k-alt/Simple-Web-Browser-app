7eb7c7734c05bba9628da8d6c72ba05976e309ca
Testing_author <testing_beta@mail.com>
2020-02-06 19:07:58 +0200

    Added a few website to the list of predefined website that are to be accesed by the user.

--- Simple WebBrowser/ViewController.swift
+++ Simple WebBrowser/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
     var progressView: UIProgressView!
     
-    
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
@@ -23,9 +22,6 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "+", style: .plain, target: self, action: #selector(openTapped))
-        
- 
                 // creating the forward, back and newPage buttons
         let goForward = UIBarButtonItem(title: "Next >", style: .plain, target: webView, action: #selector(webView.goForward))
         let goBack = UIBarButtonItem(title: "< Back", style: .plain, target: webView, action: #selector(webView.goBack))
@@ -36,7 +32,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
         
         progressView = UIProgressView(progressViewStyle: .default)
-        progressView.sizeToFit()
+        progressView.sizeToFit()            
         let progressViewButton = UIBarButtonItem(customView: progressView)
         
         toolbarItems = [progressViewButton, goBack, goForward, spacer, refresh, addNewPage]
@@ -45,7 +41,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
         // adding KVO - key value observer
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         
-        let url = URL(string: "https://www.apple.com")!
+        let url = URL(string: "https://www.google.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
@@ -54,8 +50,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
     // introducing the function which allows to open pre-defined websites
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "bbc.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Agora.md", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "BBC.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "TheGuardian.com", style: .default, handler: openPage))
     ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac, animated: true)
